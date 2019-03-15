bcca894572545d39c8af3726b3befb737531a456
Sabina Bairamava <Sabina.Bairamava@itechart-group.com>
2019-03-15 18:35:28 +0300

    add store and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import { Provider } from "./context";
+// import App from "./components/App";
+
+// ReactDOM.render(
+//   <Provider>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
+
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { Provider } from "./context";
 import App from "./components/App";
 
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
 );
+
+export default store;
