f53065b60886a2f6a5595240ea1349cdf06189c6
Sabina <sb.sabina00@gmail.com>
2019-03-17 22:43:32 +0300

    add middleware for receiving data

---
+++ src/reducers/data.js
@@ -0,0 +1,31 @@
+import { RECEIVE_DATA } from '../actions/receiveData';
+import { SORT_COMPANIES } from '../actions/sortCompanies';
+
+const initialState = {
+  loadingData: true,
+  serviceTypes: {},
+  companies: []
+};
+
+export default function data(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_DATA:
+      return {
+        ...state,
+        loadingData: false,
+        serviceTypes: action.serviceTypes,
+        companies: action.companies
+      };
+    case SORT_COMPANIES:
+      const sortedCompaniesList = [...state.companies].sort(
+        (company1, company2) =>
+          company2[action.sortBy] - company1[action.sortBy]
+      );
+      return {
+        ...state,
+        companies: sortedCompaniesList
+      };
+    default:
+      return state;
+  }
+}
