f1f61c50efa3cf1f51c93e48ea76d58ffd601a3d
Sabina <sb.sabina00@gmail.com>
2019-03-17 22:44:37 +0300

    add sort action

---
+++ src/actions/sortCompanies.js
@@ -0,0 +1,14 @@
+export const SORT_COMPANIES = 'CHANGE_FIELDS';
+
+function sortCompanies(sortBy) {
+  return {
+    type: SORT_COMPANIES,
+    sortBy
+  };
+}
+
+export function handleSortValueChange(sortBy) {
+  return dispatch => {
+    dispatch(sortCompanies(sortBy));
+  };
+}
