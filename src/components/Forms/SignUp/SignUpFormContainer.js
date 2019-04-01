9e326f308cc1847e21f0b7697cad5656b7bc3372
Anton Charnou <anton.charnou@itechart-group.com>
2019-04-01 22:28:42 +0300

    oops revert quotes

--- src/components/Forms/SignUp/SignUpFormContainer.js
+++ src/components/Forms/SignUp/SignUpFormContainer.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import SignUpForm from './SignUpForm';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import SignUpForm from "./SignUpForm";
 // TODO https://onoblog.com/absolute-path-imports-create-react-app/
-import { registerUser } from '../../../actions/authActions';
-import { handleFormChange } from '../../../actions/updateFieldsState';
+import { registerUser } from "../../../actions/authActions";
+import { handleFormChange } from "../../../actions/updateFieldsState";
 
 class SignInFormContainer extends Component {
   render() {
